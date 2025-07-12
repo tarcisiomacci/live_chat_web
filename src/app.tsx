3868cd41717970b9e50c5f20745780f445836085
Tarcisio Maciel <tarcisio.maciel@gmgroupbrasil.com.br>
2025-07-12 10:35:57 -0300

    configurações básicas feitas para rodar todo o ambiente - Clonar esse commit para novos sistemas do zero

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
---
+++ src/pages/create-room.tsx
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+type GetRoomsResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
+export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsResponse = await response.json();
+
+      return result;
+    },
+  });
+
+  return (
+    <div>
+      <div>
+        <h1>Room List</h1>
+      </div>
+
+      {isLoading && <p>Carregando...</p>}
+
+      <div className="flex flex-col gap-1">
+        {data?.map((room) => {
+          return (
+            <Link key={room.id} to={`/room/${room.id}`}>
+              {room.name}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
